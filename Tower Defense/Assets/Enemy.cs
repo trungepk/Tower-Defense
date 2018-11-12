9b088543a1d901e9669e5d53b5bb43e593b94a6b
trungepk <nevergiveup14896@gmail.com>
2018-11-12 20:11:59 +0700

    Enemy Path Finding

---
+++ Tower Defense/Assets/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex;
+
+    private void Start()
+    {
+        target = Waypoint.points[0];
+    }
+
+    private void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        if (Vector3.Distance(transform.position, target.position) <= 0.4f) 
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    private void GetNextWaypoint()
+    {
+        if (waypointIndex >= Waypoint.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        waypointIndex++;
+        target = Waypoint.points[waypointIndex];
+    }
+}
