3a57cf2be96cf5bf269ea91c6b1e6f523cddf01c
trungepk <nevergiveup14896@gmail.com>
2018-11-12 21:56:32 +0700

    Spawn Waves of Enemies

---
+++ Tower Defense/Assets/WaveSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour {
+
+    [SerializeField] Transform enemyPrefab;
+    [SerializeField] Transform startPos;
+    [SerializeField] float timeBetweenWaves = 5f;
+    [SerializeField] float countdown = 2f;
+    [SerializeField] float timeBetweenEnemies = 0.5f;
+
+    private int waveIndex;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (countdown <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(timeBetweenEnemies);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, startPos.position, Quaternion.identity);
+    }
+}
