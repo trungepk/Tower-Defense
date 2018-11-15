ad73b93391c01d21e5dc70dd944db1fd58644f53
trungepk <nevergiveup14896@gmail.com>
2018-11-15 16:26:55 +0700

    Detect and Lock on target

---
+++ Tower Defense/Assets/Scripts/DistanceComparer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceComparer : IComparer
+{
+    private Transform turret;
+
+    public DistanceComparer(Transform turret)
+    {
+        this.turret = turret;
+    }
+
+    public int Compare(object x, object y)
+    {
+        Collider xCollider = x as Collider;
+        Collider yCollider = y as Collider;
+
+        Vector3 offset = xCollider.transform.position - turret.position;
+        var xDistance = offset.sqrMagnitude;
+
+        offset = yCollider.transform.position - turret.position;
+        var yDistance = offset.sqrMagnitude;
+
+        return xDistance.CompareTo(yDistance);
+    }
+}
---
+++ Tower Defense/Assets/Scripts/Turret.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+
+public class Turret : MonoBehaviour {
+
+    [SerializeField] float range = 15f;
+    [SerializeField] LayerMask checkLayer;
+    [SerializeField] Transform partToRotate;
+    [SerializeField] float turnSpeed = 10f;
+
+    [SerializeField] Transform nearestTarget;
+	void Start () {
+		
+	}
+	
+	void Update ()
+    {
+        DetectNearestEnemy();
+        LockOnTarget();
+    }
+
+    private void DetectNearestEnemy()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, range, checkLayer);
+        Array.Sort(colliders, new DistanceComparer(transform));
+        if (colliders.Length >= 1)
+        {
+            nearestTarget = colliders[0].transform;
+        }
+        else
+        {
+            nearestTarget = null;
+        }
+    }
+
+    private void LockOnTarget()
+    {
+        if (nearestTarget)
+        {
+            Vector3 dir = nearestTarget.position - transform.position;
+            Quaternion lookRotation = Quaternion.LookRotation(dir);
+            Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+            partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+}
