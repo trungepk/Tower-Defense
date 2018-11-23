3515ae62574f9fdc9f59c63d506bf05ee7935d80
trungepk <nevergiveup14896@gmail.com>
2018-11-23 12:11:33 +0700

    Build turret

---
+++ Tower Defense/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public static BuildManager instance;
+    public GameObject standardTurretPrefab;
+
+    private GameObject turretToBuild;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }else if (instance != this)
+        {
+            instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+}
---
+++ Tower Defense/Assets/Scripts/Node.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+
+    [SerializeField] Color ableToBuildColor;
+    [SerializeField] Color unableToBuildColor;
+    [SerializeField] Vector3 positionOffset;
+
+    private GameObject turret;
+    private Renderer rend;
+    private Color startColor;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        startColor = rend.material.color;
+    }
+
+    private void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Cant build turret here");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = Instantiate(turretToBuild, transform.position + positionOffset, Quaternion.identity);
+    }
+
+    private void OnMouseEnter()
+    {
+        if (turret != null)
+        {
+            rend.material.color = unableToBuildColor;
+            return;
+        }
+        rend.material.color = ableToBuildColor;
+    }
+
+    private void OnMouseExit()
+    {
+        rend.material.color = startColor;
+    }
+}
