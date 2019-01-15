ef1775c50fca1a9bc8b1661b0a668a0242e14040
trungepk <nevergiveup14896@gmail.com>
2019-01-15 18:59:27 +0700

    Main Menu

---
+++ Tower Defense/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    [SerializeField] string levelToLoad = "Level_1";
+
+    public void Play()
+    {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
